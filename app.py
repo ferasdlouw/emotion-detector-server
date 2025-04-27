f1eebb1da7b3dff5724ae27196a117efe65e14cb
Feras Dalou <45312293+ferasdlouw@users.noreply.github.com>
2025-04-27 20:38:15 +0300

    app.py

--- app.py
+++ app.py
@@ -21,7 +21,3 @@ def analyze_emotion():
         return jsonify({'emotion': emotion})
     except Exception as e:
         return jsonify({'error': str(e)})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
-
